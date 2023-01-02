6fe2498f98221802ee04dec729469d25a4edb8df
Ivan <ivan.savkiv.igorovych@gmail.com>
2023-01-02 23:05:45 +0200

    Add Header component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./Components/Header";
 
 function App() {
-  return <h1>Hello, world!</h1>;
+  return (
+    <div>
+      <Header></Header>
+    </div>
+  );
 }
 
 export default App;
